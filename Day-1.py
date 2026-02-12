c4055b2dff225431ecd074bdcdecfd698982f541
Vutla-Nandhini <161677164+Vutla-Nandhini@users.noreply.github.com>
2026-02-12 11:03:24 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,4 @@
+print("This is Nandhini, First day of codegnan")
+print("Today I learned Introduction about Python")
+print("we learned about python History also why we use python and how it executes ")
+print("this python has multiparadigm") 
