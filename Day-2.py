fbd3030e6901e46948abd3fd4d45c73d7fbf7533
Vutla-Nandhini <161677164+Vutla-Nandhini@users.noreply.github.com>
2026-02-13 10:57:55 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,32 @@
+"""# read input numbers from user
+num=input()
+print(type(num))# checking the type of the number
+print(num)"""
+
+
+"""# implicit Type conversion
+num1 = int(input())
+val = float(input())
+print(type(num1),type(val)) # checking the type of number
+res = num1+val
+print(res)
+print(type(res))"""
+
+"""num1 = input()
+print(type(num1))
+res = num1+10
+print(res) """
+
+# To overcome above problem by using explicit conversion
+
+"""num1 = input()
+print(type(num1))
+res = num1+str(10)
+print(res) """
+
+#converting integer to float,string and boolean
+num =12
+print(float(num))
+print(str(num))
+print(bool(num))
+
