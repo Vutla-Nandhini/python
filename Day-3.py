afc32f0b03f715bcae818f880e4097d3b32cb7e5
Vutla-Nandhini <161677164+Vutla-Nandhini@users.noreply.github.com>
2026-02-16 09:22:30 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,49 @@
+
+# converting float value to integer value
+"""num = '15.9'
+print(int(float(num)))"""
+
+
+
+#checking number is zero
+"""num = int(input())
+if num == 0:
+    print("number is zero")
+print("program done") """    
+
+
+#check if number is zero or not
+"""num = int(input())
+if(num == 0):
+    print("number is zero")
+else:
+    print("number is not zero")
+print("program is done) """
+
+
+num = int(input())
+if(num != 0):
+    print("number is not zero")
+else:
+    print("number is zero")
+print("program is done")
+
+#Read two numbers find which one is bigger
+
+num1 =int(input())
+num2 = int(input())
+if num1>num2:
+    print("num1 is bigger")
+else:
+    print("num2 is bigger")
+print("program done")
+
+
+
+
+
+
+
+
+
+
